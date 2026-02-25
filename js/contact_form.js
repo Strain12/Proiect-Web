2e728f8d827b123214505663bde4f563ffab4d7a
Adrian Stan <adrianstan05@yahoo.ro>
2026-02-25 14:22:54 +0200

    Lab 1: HTML and CSS review

---
+++ js/contact_form.js
@@ -0,0 +1,10 @@
+function submitForm() {
+    const name = document.getElementById("name").value; 
+    const email = document.getElementById("email").value; 
+    const message = document.getElementById("message").value; 
+    console.log("Nume:" + name);
+    console.log("Email:" + name);
+    console.log("Message:" + name);
+
+    console.warn("Stingerea!");
+} 
